90262af237b0e73b1e634dd370cde2de540a761e
Waseemkhan950 <waseemadil950@gmail.com>
2025-04-13 02:44:57 +0500

    changed user file

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -15,23 +15,23 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./authOptions";
 
 export const getSessionUser = async () => {
-	// try {
-	// Attempt to get the server session using NextAuth configuration
-	const session = await getServerSession(authOptions);
+	try {
+		// Attempt to get the server session using NextAuth configuration
+		const session = await getServerSession(authOptions);
 
-	// Return null if no session or user data exists
-	if (!session || !session.user) return null;
+		// Return null if no session or user data exists
+		if (!session || !session.user) return null;
 
-	// Return user data and ID if authentication is successful
-	return {
-		user: session.user,
-		userId: session.user.id,
-	};
-	// } catch (error) {
-	// Log any errors that occur during session handling
-	// console.log("Error during session handling: ", error);
-	// Return null to maintain consistent error handling
-	// return null;
-	// }
+		// Return user data and ID if authentication is successful
+		return {
+			user: session.user,
+			userId: session.user.id,
+		};
+	} catch (error) {
+		// Log any errors that occur during session handling
+		console.log("Error during session handling: ", error);
+		// Return null to maintain consistent error handling
+		return null;
+	}
 };
 // Note: if during deployment, this file causes error then try to remove trycatch block and redeploy it. issue will be resolved.
